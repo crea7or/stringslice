c05369a2bcef23b92730797395de71b67876243d
crea7or <pavel.sokolov@gmail.com>
2017-04-21 18:46:01 +0300

    last tag added

--- slicestd/slicestd.cpp
+++ slicestd/slicestd.cpp
@@ -3,17 +3,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
 
 template < typename _inputIterator, typename _unaryFuntion >
 void each_tag( _inputIterator _from, _inputIterator _to, typename _inputIterator::value_type _delimiter, _unaryFuntion _function )
 {
 	std::basic_string<typename _inputIterator::value_type> tag;
-	_inputIterator _cur = _from;
-	for ( ; _cur != _to; ++_cur )
+	for ( ; _from != _to; ++_from )
 	{
-		if ( *_cur != _delimiter )
+		if ( *_from != _delimiter )
 		{
-			tag.push_back( *_cur );
+			tag.push_back( *_from );
 		}
 		else
 		{
@@ -24,28 +24,57 @@ void each_tag( _inputIterator _from, _inputIterator _to, typename _inputIterator
 			tag.clear();
 		}
 	}
+	if ( tag.size() > 0 )
+	{
+		_function( tag );
+	}
+}
+
+template < typename _inputIterator, typename _outputIterator, typename _delimiterType, typename _valueType >
+void each_tag_push( _inputIterator _from, _inputIterator _to, _delimiterType _delimiter, _outputIterator _output )
+{
+//	std::static_assert( std::is_same( typename _inputIterator::value_type, _delimiterType ));
+	_inputIterator _cur = _from;
+	for ( ; _from != _to; ++_from )
+	{
+		if ( *_from == _delimiter )
+		{
+			if ( _cur != _from )
+			{
+				_output = _valueType( _cur, _from );
+				_cur = _from;
+			}
+		}
+	}
+	if ( _cur != _from )
+	{
+		_output = _valueType( _cur, _from );
+		_cur = _from;
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	std::string test( " 498924782h7 nuncinsiunc 77826376276572  736736473467 " );
-	std::wstring wtest( L" 498924782h7 nuncinsiunc 77826376276572  736736473467 " );
+	std::string test( " 498924782h7 nuncinsiunc 77826376276572  736736473467 sdfsdfw344224 d" );
+	std::wstring wtest( L" 498924782h7 nuncinsiunc 77826376276572  736736473467 fefsdf22234 " );
 
+	std::cout << "char:" << std::endl;
 	each_tag( test.begin(), test.end(), ' ', []( std::string& tag )
 	{
 		std::cout << tag << std::endl;
 	});
 
-	each_tag( wtest.begin(), wtest.end(), ' ', []( std::wstring& tag )
+	std::cout << std::endl << "wchar:" << std::endl;
+	each_tag( wtest.begin(), wtest.end(), L' ', []( std::wstring& tag )
 	{
 		std::wcout << tag << std::endl;
 	} );
 
-// 	std::vector<int> vtest{ 0, 2, 4, 3, 4, 5, 6, 7, 8, 9, 0, 4, 6, 33, 4, 5, 66, 7, 7, 8, 9, 2, 1, 1, 2, 2, 3, 4, 5, 79, 3, 3, 2, 7, 8, 9, 9 };
-// 	each_tag( vtest.begin(), vtest.end(), 9 , []( std::string& tag )
-// 	{
-// 		std::cout << tag << std::endl;
-// 	} );
+	/*
+	std::vector< std::string > slices;
+	std::back_insert_iterator<std::vector< std::string >>  output( slices );
+	each_tag_push( test.begin(), test.end(), ' ', output );
+	*/
 
 	return 0;
 }
