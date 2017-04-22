42bc769ef40128c708dd22127cd4506588080566
crea7or <pavel.sokolov@gmail.com>
2017-04-22 04:02:53 +0300

    multichar slicer

--- slicestd/slicestd.cpp
+++ slicestd/slicestd.cpp
@@ -6,38 +6,13 @@
 #include <iterator>
 #include <algorithm>
 
-template < typename _inputIterator, typename _unaryFuntion >
-void each_tag( _inputIterator _from, _inputIterator _to, typename _inputIterator::value_type _delimiter, _unaryFuntion _function )
-{
-	_inputIterator _first = _from, _cur = _from;
-	for ( ; _from != _to; ++_from )
-	{
-		if ( *_from == _delimiter )
-		{
-			if ( _first != _cur )
-			{
-				_function( std::basic_string<typename _inputIterator::value_type>( _first, _cur ));
-			}
-			_first = ++_cur;
-		}
-		else
-		{
-			++_cur;
-		}
-	}
-	if ( _first != _cur )
-	{
-		_function( std::basic_string<typename _inputIterator::value_type>( _first, _cur ));
-	}
-}
-
 template < typename _inputIterator, typename _outputIterator>
-void each_tag_push( _inputIterator _from, _inputIterator _to, typename _inputIterator::value_type _delimiter, _outputIterator _output )
+void each_slice_push( _inputIterator _begin, _inputIterator _end, typename _inputIterator::value_type _delimiter, _outputIterator _output )
 {
-	_inputIterator _first = _from, _cur = _from;
-	for ( ; _from != _to; ++_from )
+	_inputIterator _first = _begin, _cur = _begin;
+	for ( ; _begin != _end; ++_begin )
 	{
-		if ( *_from == _delimiter )
+		if ( *_begin == _delimiter )
 		{
 			if ( _first != _cur )
 			{
@@ -56,96 +31,99 @@ void each_tag_push( _inputIterator _from, _inputIterator _to, typename _inputIte
 	}
 }
 
-
-// in process
-
-template < typename _inputIterator, typename _unaryFuntion >
-void each_tag( _inputIterator _from, _inputIterator _to, const std::basic_string<typename _inputIterator::value_type> _delimiter, _unaryFuntion _function )
+template < typename _inputIterator, typename _unaryFunction >
+void each_slice( _inputIterator _begin, _inputIterator _end, typename _inputIterator::value_type _delimiter, _unaryFunction _function )
 {
-	//std::basic_string<typename _inputIterator::value_type> tag;
-	//std::basic_string<typename _inputIterator::value_type> searchFor( _delimiter );
-
-	size_t sizeCurrent = 0;
-	size_t stringSize = _delimiter.size();
-//	while ( _delimiter[ stringSize ] != 0 )
-//	{
-//		++stringSize;
-//	};
-
-	_inputIterator _first = _from, _cur = _from;
-
-	for ( ; _from != _to; ++_from )
+	_inputIterator _first = _begin, _cur = _begin;
+	for ( ; _begin != _end; ++_begin )
 	{
-		if ( *_from == _delimiter[ sizeCurrent ] )
+		if ( *_begin == _delimiter )
 		{
-			tag.push_back( *_from );
+			if ( _first != _cur )
+			{
+				_function( std::basic_string<typename _inputIterator::value_type>( _first, _cur ));
+			}
+			_first = ++_cur;
 		}
 		else
 		{
-
-			if ( tag.size() > 0 )
-			{
-				_function( tag );
-			}
-			tag.clear();
-			sizeCurrent = 0;
+			++_cur;
 		}
 	}
-	if ( tag.size() > 0 )
+	if ( _first != _cur )
 	{
-		_function( tag );
+		_function( std::basic_string<typename _inputIterator::value_type>( _first, _cur ));
 	}
 }
 
-template < typename _inputIterator, typename _unaryFuntion >
-void each_tag( _inputIterator _from, _inputIterator _to, const typename _inputIterator::value_type* _delimiter, _unaryFuntion _function )
+template < typename _inputIterator, typename _unaryFunction >
+void each_slice( _inputIterator _begin, _inputIterator _end, typename _inputIterator::value_type* _delimiter, _unaryFunction _function )
 {
-	std::basic_string<typename _inputIterator::value_type> tag;
-	//std::basic_string<typename _inputIterator::value_type> searchFor( _delimiter );
-
-	size_t sizeCurrent = 0, stringSize = 0;
+	size_t indexCurrent = 0, stringSize = 0;
 	while ( _delimiter[ stringSize ] != 0 )
 	{
 		++stringSize;
 	};
 
-	_inputIterator _first = _from, _cur = _from;
-
-	for ( ; _from != _to; ++_from )
+	if ( stringSize > 1 )
 	{
-		if ( *_from == _delimiter[ sizeCurrent ] )
+		--stringSize;
+		_inputIterator _first = _begin, _final = _begin, _last, _fromPattern;
+		for ( ; _begin != _end; ++_begin )
 		{
-			tag.push_back( *_from );
-		}
-		else
-		{
-
-			if ( tag.size() > 0 )
+			if ( *_begin == _delimiter[ indexCurrent ] )
 			{
-				_function( tag );
+				_last = _fromPattern = _begin;
+				while ( ++_fromPattern != _end && *_fromPattern == _delimiter[ ++indexCurrent ] )
+				{
+					if ( indexCurrent == stringSize )
+					{
+						if ( _first != _last )
+						{
+							_function( std::basic_string<typename _inputIterator::value_type>( _first, _last ) );
+						}
+						_begin = _fromPattern++;
+						_final = _first = _fromPattern;
+						break;
+					}
+				};
+				indexCurrent = 0;
 			}
-			tag.clear();
-			sizeCurrent = 0;
+		}
+
+		// last slice
+		if ( _final != _end )
+		{
+			_function( std::basic_string<typename _inputIterator::value_type>( _final, _end ) );
 		}
 	}
-	if ( tag.size() > 0 )
+	else if ( stringSize == 1 )
 	{
-		_function( tag );
+		// only one symbol in delimiter, use simplier variant
+		each_slice( _begin, _end, _delimiter[ 0 ], _function );
 	}
+	/*
+	else
+	{
+		// only one slice, no delimiters supplied
+		_function( std::basic_string<typename _inputIterator::value_type>( _begin, _end ));
+	}
+	*/
 }
-
 
 
 int main(int argc, char* argv[])
 {
-	std::string test( " 498924782h7 nuncinsiunuc 77826376276572  nu736736473467 sdfsdfw344224 d" );
-	std::wstring wtest( L" 498nu924782h7 nuncinsiunc 778263762765nu72  73673nuu6473467 fefsdf22234 " );
+	std::string test( "cpp.kw1 source.cpp.kw2 source.cpp.kw3 sourcesource.cpp.kw4_source.cpp.co1 source.cpp.co2 source.cpp source.cpp.es0 source" );
+	std::wstring wtest( test.begin(), test.end() );
+
+	size_t count = 0;
 
 #pragma region single char slices
 
-	size_t count = 0;
-	std::cout << "char:" << std::endl;
-	each_tag( test.begin(), test.end(), ' ', [&count]( std::string& tag )
+
+	std::cout << std::endl << "char (' '):" << std::endl;
+	each_slice( test.begin(), test.end(), ' ', [&count]( std::string& tag )
 	{
 		std::cout << tag << std::endl;
 		++count;
@@ -153,10 +131,10 @@ int main(int argc, char* argv[])
 	std::cout << "total tags: " << count << std::endl;
 
 
-	std::cout << std::endl << "char + outputIterator:" << std::endl;
+	std::cout << std::endl << "char + outputIterator (' '):" << std::endl;
 	std::vector< std::string > slices;
 	std::back_insert_iterator<std::vector< std::string >>  output( slices );
-	each_tag_push( test.begin(), test.end(), ' ', output ); // <- char to wchar implict convertsion is possible
+	each_slice_push( test.begin(), test.end(), ' ', output ); // <- char to wchar implict convertsion is possible
 
 	for_each( slices.begin(), slices.end(), []( std::string& tag )
 	{
@@ -165,19 +143,19 @@ int main(int argc, char* argv[])
 	std::cout << "total tags: " << slices.size() << std::endl;
 
 
-	size_t wcount = 0;
-	std::cout << std::endl << "wchar:" << std::endl;
-	each_tag( wtest.begin(), wtest.end(), L' ', [ &wcount ]( std::wstring& tag )
+	count = 0;
+	std::cout << std::endl << std::endl << "wchar (' '):" << std::endl;
+	each_slice( wtest.begin(), wtest.end(), ' ', [ &count ]( std::wstring& tag )
 	{
 		std::wcout << tag << std::endl;
-		++wcount;
+		++count;
 	});
-	std::cout << "total tags: " << wcount << std::endl;
+	std::cout << "total tags: " << count << std::endl;
 
-	std::cout << std::endl << "wchar + outputIterator:" << std::endl;
+	std::cout << std::endl << "wchar + outputIterator (' '):" << std::endl;
 	std::vector< std::wstring > wslices;
 	std::back_insert_iterator<std::vector< std::wstring >> woutput( wslices );
-	each_tag_push( wtest.begin(), wtest.end(), L' ', woutput );
+	each_slice_push( wtest.begin(), wtest.end(), L' ', woutput );
 
 	for_each( wslices.begin(), wslices.end(), []( std::wstring& tag )
 	{
@@ -186,16 +164,40 @@ int main(int argc, char* argv[])
 	std::cout << "total tags: " << wslices.size() << std::endl;
 #pragma endregion
 
-	/*
-	size_t mcount = 0;
-	std::cout << "char:" << std::endl;
-	each_tag( test.begin(), test.end(), "nu", [ &mcount ]( std::string& tag )
+
+#pragma region multi chars slices
+
+	count = 0;
+	std::cout << std::endl << "multi char 1 (cpp):" << std::endl;
+	each_slice( test.begin(), test.end(), "cpp", [ &count ]( std::string& tag )
 	{
 		std::cout << tag << std::endl;
-		++mcount;
+		++count;
 	} );
 	std::cout << "total tags: " << count << std::endl;
-	*/
+
+
+	count = 0;
+	std::cout << std::endl << "multi char 2 (source):" << std::endl;
+	each_slice( test.begin(), test.end(), "source", [ &count ]( std::string& tag )
+	{
+		std::cout << tag << std::endl;
+		++count;
+	} );
+	std::cout << "total tags: " << count << std::endl;
+
+
+	count = 0;
+	std::cout << std::endl << "multi char as one (' '):" << std::endl;
+	each_slice( test.begin(), test.end(), " ", [ &count ]( std::string& tag )
+	{
+		std::cout << tag << std::endl;
+		++count;
+	} );
+	std::cout << "total tags: " << count << std::endl;
+
+#pragma endregion
+
 	return 0;
 }
 
